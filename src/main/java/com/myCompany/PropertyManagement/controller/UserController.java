5e32e42be447907f753d454676c8969660ec70ae
HimanshuMaheshwari <himanshu.maheshwari54@gmail.com>
2022-05-02 15:39:57 +0530

    implemented swagger api documentation

--- src/main/java/com/myCompany/PropertyManagement/controller/UserController.java
+++ src/main/java/com/myCompany/PropertyManagement/controller/UserController.java
@@ -3,6 +3,8 @@ package com.myCompany.PropertyManagement.controller;
 
 import com.myCompany.PropertyManagement.dto.UserDTO;
 import com.myCompany.PropertyManagement.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Path;
 import javax.validation.Valid;
 
 @RestController
@@ -21,15 +24,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @ApiOperation(value = "register", notes = "This method is used for user registration")
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> register(@ApiParam(
+            name = "userDTO",
+            type = "userDTO",
+            value = "user data",
+            example = "user information",
+            required = true
+    ) @Valid @RequestBody UserDTO userDTO){
 
         userDTO = userService.register(userDTO);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
+    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
     public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO){
         userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
