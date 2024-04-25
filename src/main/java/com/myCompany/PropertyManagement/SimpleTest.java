3af51d4a5ee99e31a818ea70c94c50b4fa5ed07c
HimanshuMaheshwari45 <66906908+HimanshuMaheshwari45@users.noreply.github.com>
2024-04-25 23:32:08 +0530

    Update SimpleTest.java
    new commit for pull request.

--- src/main/java/com/myCompany/PropertyManagement/SimpleTest.java
+++ src/main/java/com/myCompany/PropertyManagement/SimpleTest.java
@@ -4,6 +4,7 @@ public class SimpleTest {
   public static void main(String[] args){
     System.out.println("this change is for sample of pull/featch");
     System.out.println("this change is for sample of pull/featch from git command");
+    System.out.println("this change is for sample of pull request/featch from git command");
 
     
     
