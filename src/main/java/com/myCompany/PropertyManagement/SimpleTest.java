efc4bfb5f6f2bb3da59148bf642cdb9a2addd4af
HimanshuMaheshwari45 <66906908+HimanshuMaheshwari45@users.noreply.github.com>
2024-04-25 23:21:30 +0530

    Update SimpleTest.java
    updated the simple test class

--- src/main/java/com/myCompany/PropertyManagement/SimpleTest.java
+++ src/main/java/com/myCompany/PropertyManagement/SimpleTest.java
@@ -1,4 +1,9 @@
 package com.myCompany.PropertyManagement;
 
 public class SimpleTest {
+  public static void main(String[] args){
+    System.out.println("this change is for sample of pull/featch")
+    
+  }
+  
 }
